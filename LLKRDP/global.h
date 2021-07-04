53772893c902f23da006bd649212280527a3836d
HunterPlus <zlw08140957@gmail.com>
2021-07-04 13:44:42 +0800

    Create global.h

---
+++ LLKRDP/global.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define TOKENSIZ  100
+
+typedef struct Token {
+    int type;
+    char text[TOKENSIZ];
+} Token;
+
+enum {
+    TEOF, NAME, COMMA, LBRACK, RBRACK, EQUALS
+};
+
